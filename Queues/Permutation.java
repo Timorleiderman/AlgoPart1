597d163c769f06513e4688710ee4dfaa2431bf70
Timorleiderman <Timorleiderman@gmail.com>
2021-09-21 23:09:00 +0300

    completed week 2

--- Queues/Permutation.java
+++ Queues/Permutation.java
@@ -5,7 +5,6 @@
  **************************************************************************** */
 
 import edu.princeton.cs.algs4.StdIn;
-import edu.princeton.cs.algs4.StdRandom;
 
 public class Permutation {
     public static void main(String[] args) {
@@ -18,15 +17,9 @@ public class Permutation {
             if (k == 0) {
                 return;
             }
-            if (k > randQ.size()) {
-                randQ.enqueue(str);
-            }
-            else if (StdRandom.uniform() < (k / n)) {
-                randQ.dequeue();
-                randQ.enqueue(str);
-            }
+            randQ.enqueue(str);
         }
-        while (!randQ.isEmpty()) {
+        for (int i = 0; i < k; i++) {
             System.out.println(randQ.dequeue());
         }
     }
