2fb43dfe79f035ac6dd90196db91842b59fffb80
Timorleiderman <Timorleiderman@gmail.com>
2021-09-29 14:48:40 +0300

    Create PointSET.java

---
+++ kdtree/PointSET.java
@@ -0,0 +1,93 @@
+/* *****************************************************************************
+ *  Name:
+ *  Date:
+ *  Description:
+ **************************************************************************** */
+
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+import edu.princeton.cs.algs4.SET;
+import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdDraw;
+
+public class PointSET {
+
+    private final SET<Point2D> set;
+
+    // construct an empty set of points
+    public PointSET() {
+        set = new SET<Point2D>();
+    }
+
+    // is the set empty?
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    // number of points in the set
+    public int size() {
+        return set.size();
+    }
+
+    // add the point to the set (if it is not already in the set)
+    public void insert(Point2D p) {
+        if (p == null) {
+            throw new IllegalArgumentException();
+        }
+        set.add(p);
+    }
+
+    // does the set contain point p?
+    public boolean contains(Point2D p) {
+        if (p == null) {
+            throw new IllegalArgumentException();
+        }
+        return set.contains(p);
+    }
+
+    // draw all points to standard draw
+    public void draw() {
+        for (Point2D point : set) {
+            StdDraw.point(point.x(), point.y());
+        }
+    }
+
+    // all points that are inside the rectangle (or on the boundary)
+    public Iterable<Point2D> range(RectHV rect) {
+        if (rect == null) {
+            throw new IllegalArgumentException();
+        }
+        Stack<Point2D> pointStack = new Stack<Point2D>();
+        for (Point2D point : set) {
+            if (rect.contains(point)) {
+                pointStack.push(point);
+            }
+        }
+        return pointStack;
+    }
+
+    // a nearest neighbor in the set to point p; null if the set is empty
+    public Point2D nearest(Point2D p) {
+        if (p == null) {
+            throw new IllegalArgumentException();
+        }
+
+        Point2D nearestPoint = null;
+
+        for (Point2D point : set) {
+            if (nearestPoint == null) {
+                nearestPoint = point;
+            }
+
+            if (point.distanceSquaredTo(p) < nearestPoint.distanceSquaredTo(p)) {
+                nearestPoint = point;
+            }
+        }
+        return nearestPoint;
+    }
+
+    public static void main(String[] args) {
+        PointSET test = new PointSET();
+        test.insert(new Point2D(0.5, 0.5));
+    }
+}
